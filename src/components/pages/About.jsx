844906d13be1c899d0657bae1b31eea05712673a
Yash <iam.007yash@gmail.com>
2024-08-08 09:00:32 +0530

    added sound file and fixed css for about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,42 +1,56 @@
 import React from "react";
-import image from "./images/yash.png"
+import profile from "./images/swati.png"
+import cloud from "./images/musiccloud.png"
+import mountain from "./images/singinglady.png"
+import chilli from "./images/chillies.png"
+import music from "./images/music.png"
+import audio from "./images/music.mp3"
+
+import { NavLink } from "react-router-dom";
+
 
 import "./About.css"
 
 export const About = () => {
+
+  const start = () => {
+    new Audio(audio).play();
+  }
   return (
-    <div class="about">
-      <div class="top-container">
-        <img class="top-cloud" src="" alt="cloud-image" />
-        <div class="text">
-          <h1 class="head">I'm Swati</h1>
+    <div className="about">
+      <div className="top-container" onClick={start}>
+        <img className="top-cloud1" src={cloud} alt="cloud-image"/>
+        <img className="top-cloud2" src={cloud} alt="cloud-image" />
+        <div className="text">
+          <h1 className="head">I'm Swati</h1>
           <h2>
-            a <span class="pro">pro</span>grammer.
+            a <span className="pro">Singer</span> & Musician.
           </h2>
         </div>
 
-        <img class="bottom-cloud" src="images/cloud.png" alt="cloud-image" />
-        <img class="mountain" src="images/mountain.png" alt="cloud-image" />
+        <img className="bottom-cloud1" src={cloud} alt="cloud-image" />
+        <img className="bottom-cloud2" src={cloud} alt="cloud-image" />
+        <img className="mountain" src={mountain} alt="cloud-image" />
       </div>
-      <div class="middle-container">
-        <div class="profile">
-          <img src="images/yash.png" alt="swati" />
+      <div className="middle-container">
+        <div className="profile">
+          <img src={profile} alt="swati" />
           <h2>Hello.</h2>
-          <p class="intro">I am a Programmer Analyst at Cognizant. And I ❤ coding and traveling.</p>
+          <p className="intro">I am a Musician. And I ❤ Singing and traveling.</p>
         </div>
 
 
-        <div class="skills">
+        <div className="skills">
           <h2>My Skills.</h2>
-          <div class="skill-row">
-            <img class="computer" src="images/computer-modified.png" alt="" />
+          <div className="skill-row">
+            <img className="computer" src={music} alt="" />
 
-            <h3>Full Stack Developer (Design & Development)</h3>
-            <p>I started learning code when i was in my 3rd year of my college I wanted to make my own applicion and websites. Over time i am learning skills and technologies.</p>
+            <h3>Singer (Music and Singing)</h3>
+            <p>I started learning music since my childhood. I want to create my own album and really keen to share my knowledge with the upcoming generations. Currently i am pursuing Masters in Music Vocal from Central University of Punjab.</p>
 
           </div>
-          <div class="skill-row">
-            <img class="chillies" src="images/chillies.png" alt="" />
+          <div className="skill-row">
+            <img className="chillies" src={chilli} alt="" />
 
             <h3>Love for Foods</h3>
             <p>But i am really good at eating and exploring differnt kind of foods, travelling a lot of places and I never get bored of them.</p>
@@ -44,11 +58,11 @@ export const About = () => {
           </div>
         </div>
 
-        <div class="contact-me">
+        <div className="contact-me">
           <h2>Get In Touch</h2>
-          <h3>If You love coding as i do.</h3>
-          <p class="contact-message">Let's talk about how awsome this is. We can share our ideas on this.</p>
-          <a class="btn" href="bittukr.0000@gmail.com">CONTACT ME</a>
+          <h3>If You really looking for an awsome singer or any colllab.</h3>
+          <p className="contact-message">Let's talk about how awsome this is. We can share our ideas on this.</p>
+          <NavLink to="/contact" classNameName="contact">CONTACT ME</NavLink>
         </div>
       </div>
     </div>
